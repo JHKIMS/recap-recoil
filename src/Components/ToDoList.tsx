7c4767220c4ac76fa31044e9dc61fe8d9ffc55ea
JHKIMS <wjdgns5131@gmail.com>
2023-07-25 11:03:21 +0900

    Recap-Recoil useRecoilState사용해서 Todo추가

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -1,12 +1,25 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+
+const todoState = atom<ITodo[]>({
+  key: "todo",
+  default: [],
+})
 
 interface IForm {
   todo: string;
 }
+interface ITodo{
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
 
 function ToDoList() {
+  const [todos, setTodos] = useRecoilState(todoState);
   const { register, handleSubmit, setValue } = useForm<IForm>({});
-  const handleValid = (data: IForm) => {
+  const handleValid = ({todo}: IForm) => {
+    setTodos(oldTodos=>[{text:todo, id:Date.now(), category:"TO_DO"},...oldTodos]);
     setValue("todo","");
   };
   return (
@@ -20,6 +33,11 @@ function ToDoList() {
         />
         <button>Add</button>
       </form>
+      <ul>
+        {todos.map((todo)=>(
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
