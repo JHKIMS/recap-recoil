8f6fe3f1aacbaa5df54eea1b5679990bb3cbe71c
JHKIMS <wjdgns5131@gmail.com>
2023-07-27 10:16:08 +0900

    Recap-Recoil Selector로 카테고리 분류-완료

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -1,24 +1,37 @@
-import { useRecoilValue } from "recoil";
-import { todoSelector, todoState } from "./atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, todoSelector, todoState } from "./atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const todos = useRecoilValue(todoState);
-  const selectorOutput = useRecoilValue(todoSelector);
-  const [todo, doing, done] = useRecoilValue(todoSelector);
-  // console.log(todos);
+  const todos = useRecoilValue(todoState); // console.log용
+  const selectorOutput = useRecoilValue(todoSelector); // console.log용
+  const todo = useRecoilValue(todoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event:React.FormEvent<HTMLSelectElement>) =>{
+    setCategory(event.currentTarget.value);
+  }
+
+  console.log(todos);
   console.log(selectorOutput);
   return (
     <div>
       <h1>ToDos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">ToDo</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>ToDo</h2>
+      {todo?.map((todoEle)=>(
+        <ToDo key={todoEle.id} {...todoEle} />
+      ))}
+      {/* <h2>ToDo</h2>
       <ul>
         {todo.map((todo) => (
           <ToDo key={todo.id} {...todo} />
-        ))}
+        ))} */}
         {/* 이렇게 써도 잘 돌아가는 이유는 todos배열의 
           todo원소 하나하나가 todo컴포넌트에 필요한 prop과 같은 모양이기 때문이다. 
           두 가지다 같은 타입을 바라보고 있다.*/}
@@ -27,8 +40,8 @@ function ToDoList() {
            todo가 같은 prop을 가지고 있기 때문에 가능한 코드*/}
         {/* todoState : ITodo타입 
            ToDo컴포넌트 : ITodo타입의 prop을 받는다.*/}
-      </ul>
-      <hr />
+      {/* </ul> */}
+      {/* <hr />
 
       <h2>Doing</h2>
       <ul>
@@ -43,7 +56,7 @@ function ToDoList() {
         {done.map((todo) => (
           <ToDo key={todo.id} {...todo} />
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
